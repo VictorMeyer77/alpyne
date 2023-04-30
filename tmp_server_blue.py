31d5ffdb4c72dbcfcf9b0e913a5a777042a8a7b2
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-30 21:31:05 +0200

    add test bluetooth

---
+++ tmp_server_blue.py
@@ -0,0 +1,20 @@
+import bluetooth
+
+hostMACAddress = 'B8:27:EB:2D:A1:31'
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+try:
+    client, clientInfo = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data)
+except:
+    print("Closing socket")
+    client.close()
+    s.close()
