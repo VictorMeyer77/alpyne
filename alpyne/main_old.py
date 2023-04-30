d1c0c6526ed17e38ebc84c90335a3cc12bf87312
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-30 18:41:51 +0200

    test move

--- alpyne/main_old.py
+++ alpyne/main_old.py
@@ -14,20 +14,21 @@ motor_two_pins = config["motor.two.pins"]
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-GPIO.setup(int(motor_one_pins["Enable"]), GPIO.OUT)
-GPIO.setup(int(motor_one_pins["InputOne"]), GPIO.OUT)
-GPIO.setup(int(motor_one_pins["InputTwo"]), GPIO.OUT)
 
-GPIO.setup(int(motor_two_pins["Enable"]), GPIO.OUT)
-GPIO.setup(int(motor_two_pins["InputOne"]), GPIO.OUT)
-GPIO.setup(int(motor_two_pins["InputTwo"]), GPIO.OUT)
+def init():
+    GPIO.setup(int(motor_one_pins["Enable"]), GPIO.OUT)
+    GPIO.setup(int(motor_one_pins["InputOne"]), GPIO.OUT)
+    GPIO.setup(int(motor_one_pins["InputTwo"]), GPIO.OUT)
 
-# Voir aide dans le tuto
-M1_Vitesse = GPIO.PWM(int(motor_one_pins["Enable"]), 100)
-M2_Vitesse = GPIO.PWM(int(motor_two_pins["Enable"]), 100)
-M1_Vitesse.start(100)
-M2_Vitesse.start(100)
+    GPIO.setup(int(motor_two_pins["Enable"]), GPIO.OUT)
+    GPIO.setup(int(motor_two_pins["InputOne"]), GPIO.OUT)
+    GPIO.setup(int(motor_two_pins["InputTwo"]), GPIO.OUT)
 
+    # Voir aide dans le tuto
+    M1_Vitesse = GPIO.PWM(int(motor_one_pins["Enable"]), 100)
+    M2_Vitesse = GPIO.PWM(int(motor_two_pins["Enable"]), 100)
+    M1_Vitesse.start(100)
+    M2_Vitesse.start(100)
 
 def motor_forward(motor_pins):
     print("GPIO {} to HIGH and {} to LOW".format(motor_pins["InputOne"], motor_pins["InputTwo"]))
@@ -70,7 +71,8 @@ def stop():
     motor_stop(motor_one_pins)
     motor_stop(motor_two_pins)
 
-
+init()
 stop()
 forward()
-sleep(5)
+sleep(3)
+stop()
