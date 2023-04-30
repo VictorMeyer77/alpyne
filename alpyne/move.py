13da2183f3f75e7cf74e1d5970ddec157c1c3683
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-30 17:16:46 +0200

    convert gpio to int

--- alpyne/move.py
+++ alpyne/move.py
@@ -7,31 +7,31 @@ class Move:
         self.motor_one_pins = motor_one_pins
         self.motor_two_pins = motor_two_pins
         self._init_gpio_output()
-        GPIO.PWM(motor_one_pins["Enable"], 100).start(100)
-        GPIO.PWM(motor_two_pins["Enable"], 100).start(100)
+        GPIO.PWM(int(motor_one_pins["Enable"]), 100).start(100)
+        GPIO.PWM(int(motor_two_pins["Enable"]), 100).start(100)
 
     def _init_gpio_output(self):
-        GPIO.setup(self.motor_one_pins["Enable"], GPIO.OUT)
-        GPIO.setup(self.motor_one_pins["InputOne"], GPIO.OUT)
-        GPIO.setup(self.motor_one_pins["InputTwo"], GPIO.OUT)
-        GPIO.setup(self.motor_two_pins["Enable"], GPIO.OUT)
-        GPIO.setup(self.motor_two_pins["InputOne"], GPIO.OUT)
-        GPIO.setup(self.motor_two_pins["InputTwo"], GPIO.OUT)
+        GPIO.setup(int(self.motor_one_pins["Enable"]), GPIO.OUT)
+        GPIO.setup(int(self.motor_one_pins["InputOne"]), GPIO.OUT)
+        GPIO.setup(int(self.motor_one_pins["InputTwo"]), GPIO.OUT)
+        GPIO.setup(int(self.motor_two_pins["Enable"]), GPIO.OUT)
+        GPIO.setup(int(self.motor_two_pins["InputOne"]), GPIO.OUT)
+        GPIO.setup(int(self.motor_two_pins["InputTwo"]), GPIO.OUT)
 
     @staticmethod
     def motor_forward(motor_pins):
-        GPIO.output(motor_pins["InputOne"], GPIO.HIGH)
-        GPIO.output(motor_pins["InputTwo"], GPIO.LOW)
+        GPIO.output(int(motor_pins["InputOne"]), GPIO.HIGH)
+        GPIO.output(int(motor_pins["InputTwo"]), GPIO.LOW)
 
     @staticmethod
     def motor_backward(motor_pins):
-        GPIO.output(motor_pins["InputOne"], GPIO.LOW)
-        GPIO.output(motor_pins["InputTwo"], GPIO.HIGH)
+        GPIO.output(int(motor_pins["InputOne"]), GPIO.LOW)
+        GPIO.output(int(motor_pins["InputTwo"]), GPIO.HIGH)
 
     @staticmethod
     def motor_stop(motor_pins):
-        GPIO.output(motor_pins["InputOne"], GPIO.LOW)
-        GPIO.output(motor_pins["InputTwo"], GPIO.LOW)
+        GPIO.output(int(motor_pins["InputOne"]), GPIO.LOW)
+        GPIO.output(int(motor_pins["InputTwo"]), GPIO.LOW)
 
     def forward(self):
         self.motor_forward(self.motor_one_pins)
