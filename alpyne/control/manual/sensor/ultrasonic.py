cf3721bbab068148bd7aa1602e3793785c83d06e
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-01 20:26:25 +0200

    sensor automatic control: add ultrasonic sensor

---
+++ alpyne/control/manual/sensor/ultrasonic.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def ultrasound_distance(ultrasonic_pins):
+    GPIO.output(ultrasonic_pins["Trigger"], True)
+    time.sleep(0.00001)
+    GPIO.output(ultrasonic_pins["Trigger"], False)
+    start_time = time.time()
+    stop_time = time.time()
+    while GPIO.input(ultrasonic_pins["Echo"]) == 0:
+        start_time = time.time()
+    while GPIO.input(ultrasonic_pins["Echo"]) == 1:
+        stop_time = time.time()
+    return (stop_time - start_time) * 34300.0 / 2.0
