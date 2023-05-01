9af22a3a01f2878a04b56f867b192966179bd157
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-01 21:33:24 +0200

    ultrasonic automatic: test real

--- alpyne/control/automatic/a01/launcher.py
+++ alpyne/control/automatic/a01/launcher.py
@@ -2,14 +2,15 @@ import queue
 import threading
 from move import Move
 from control.automatic.a01.controller import Controller
-from control.sensor.ultrasonic import ultrasonic_loop
+from control.sensor.ultrasonic import Ultrasonic
 
 
 def launch(config):
     ultrasonic_queue = queue.Queue()
     move = Move(config["motor.one.pins"], config["motor.two.pins"])
+    ultrasonic = Ultrasonic(config["ultrasonic.pins"], ultrasonic_queue)
     controller = Controller(move, ultrasonic_queue)
     con_thread = threading.Thread(target=controller.run)
-    ult_thread = threading.Thread(target=ultrasonic_loop, args=(config["ultrasonic.pins"], ultrasonic_queue))
+    ult_thread = threading.Thread(target=ultrasonic.start)
     con_thread.start()
     ult_thread.start()
--- alpyne/control/sensor/ultrasonic.py
+++ alpyne/control/sensor/ultrasonic.py
@@ -2,20 +2,29 @@ import RPi.GPIO as GPIO
 import time
 
 
-def ultrasonic_loop(ultrasonic_pins, queue):
-    while True:
-        tmp = ultrasonic_distance(ultrasonic_pins)
-        queue.put(tmp)
+class Ultrasonic:
 
+    def __init__(self, ultrasonic_pins, queue):
 
-def ultrasonic_distance(ultrasonic_pins):
-    GPIO.output(int(ultrasonic_pins["Trigger"]), True)
-    time.sleep(0.00001)
-    GPIO.output(int(ultrasonic_pins["Trigger"]), False)
-    start_time = time.time()
-    stop_time = time.time()
-    while GPIO.input(int(ultrasonic_pins["Echo"])) == 0:
+        self.ultrasonic_pins = ultrasonic_pins
+        self.queue = queue
+        GPIO.setup(self.ultrasonic_pins["Trigger"], GPIO.OUT)
+        GPIO.setup(self.ultrasonic_pins["Echo"], GPIO.IN)
+
+    def start(self):
+        while True:
+            tmp = self._compute_distance()
+            print(tmp)
+            self.queue.put(tmp)
+
+    def _compute_distance(self):
+        GPIO.output(int(self.ultrasonic_pins["Trigger"]), True)
+        time.sleep(0.00001)
+        GPIO.output(int(self.ultrasonic_pins["Trigger"]), False)
         start_time = time.time()
-    while GPIO.input(int(ultrasonic_pins["Echo"])) == 1:
         stop_time = time.time()
-    return (stop_time - start_time) * 34300.0 / 2.0
+        while GPIO.input(int(self.ultrasonic_pins["Echo"])) == 0:
+            start_time = time.time()
+        while GPIO.input(int(self.ultrasonic_pins["Echo"])) == 1:
+            stop_time = time.time()
+        return (stop_time - start_time) * 34300.0 / 2.0
