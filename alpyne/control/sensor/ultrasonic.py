054fa5130744b25dc7fc9a1eda5f0057829f03f1
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-01 21:28:46 +0200

    ultrasonic automatic: test real

--- alpyne/control/sensor/ultrasonic.py
+++ alpyne/control/sensor/ultrasonic.py
@@ -9,13 +9,13 @@ def ultrasonic_loop(ultrasonic_pins, queue):
 
 
 def ultrasonic_distance(ultrasonic_pins):
-    GPIO.output(ultrasonic_pins["Trigger"], True)
+    GPIO.output(int(ultrasonic_pins["Trigger"]), True)
     time.sleep(0.00001)
-    GPIO.output(ultrasonic_pins["Trigger"], False)
+    GPIO.output(int(ultrasonic_pins["Trigger"]), False)
     start_time = time.time()
     stop_time = time.time()
-    while GPIO.input(ultrasonic_pins["Echo"]) == 0:
+    while GPIO.input(int(ultrasonic_pins["Echo"])) == 0:
         start_time = time.time()
-    while GPIO.input(ultrasonic_pins["Echo"]) == 1:
+    while GPIO.input(int(ultrasonic_pins["Echo"])) == 1:
         stop_time = time.time()
     return (stop_time - start_time) * 34300.0 / 2.0
