d25b03e3939014426af12f49370a88c9369b163b
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-01 18:14:28 +0200

    bluetooth control: add tmp print

--- alpyne/control/manual/server/controller.py
+++ alpyne/control/manual/server/controller.py
@@ -13,6 +13,7 @@ class Controller:
                 self.execute_command(command)
 
     def execute_command(self, command):
+        print(command)
         if command == "o":
             self.move.forward()
         elif command == "l":
