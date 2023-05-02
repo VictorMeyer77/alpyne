4a451ee9eebf840218f3657ca27855ff83f7cc5b
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-02 13:45:52 +0200

    ultrasonic automatic: test real

--- alpyne/control/automatic/a01/controller.py
+++ alpyne/control/automatic/a01/controller.py
@@ -15,11 +15,11 @@ class Controller:
                 print(tmp)
                 if tmp > 50.0:
                     self.move.forward()
-                    time.sleep(3)
+                    #time.sleep(3)
                 else:
                     self.move.right()
-                    time.sleep(3)
-                    self.move.stop()
+                    #time.sleep(3)
+                    #self.move.stop()
         except KeyboardInterrupt:
             pass
 
