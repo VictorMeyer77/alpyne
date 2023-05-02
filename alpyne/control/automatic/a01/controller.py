84902a4bbf39cb25c8f3986d2d4c6636d0313dd0
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-02 19:44:31 +0200

    ultrasonic automatic control: test real

--- alpyne/control/automatic/a01/controller.py
+++ alpyne/control/automatic/a01/controller.py
@@ -8,14 +8,10 @@ class Controller:
         self.move = move
 
     def run(self, environment_info):
-        tmp = float(environment_info["ultrasonic_distance"])
-        print(tmp)
-        if tmp > 30.0:
+        distance = float(environment_info["ultrasonic_distance"])
+        print(distance)
+        if distance > 40.0:
             self.move.forward()
-            print("move forward")
         else:
             self.move.right()
-            time.sleep(0.3)
-            print("move right")
-            self.move.stop()
 
