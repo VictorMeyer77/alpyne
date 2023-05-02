f7d4f210f8e9ed0588afcd74c488e37349aba969
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-02 13:38:00 +0200

    ultrasonic controller: test add setmode

--- alpyne/control/sensor/ultrasonic.py
+++ alpyne/control/sensor/ultrasonic.py
@@ -8,7 +8,6 @@ class Ultrasonic:
 
         self.ultrasonic_pins = ultrasonic_pins
         self.queue = queue
-        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(int(self.ultrasonic_pins["Trigger"]), GPIO.OUT)
         GPIO.setup(int(self.ultrasonic_pins["Echo"]), GPIO.IN)
 
