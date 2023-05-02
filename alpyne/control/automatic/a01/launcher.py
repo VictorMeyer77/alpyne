b59a8d160b70f02fb2e37c7607d230d05cbe6785
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-02 18:18:59 +0200

    ultrasonic automatic control: test real

--- alpyne/control/automatic/a01/launcher.py
+++ alpyne/control/automatic/a01/launcher.py
@@ -9,13 +9,13 @@ from control.sensor.ultrasonic import Ultrasonic
 def launch(config):
     ultrasonic_queue = queue.Queue()
     move = Move(config["motor.one.pins"], config["motor.two.pins"])
-    move.forward()
-    time.sleep(5)
-    """ultrasonic = Ultrasonic(config["ultrasonic.pins"], ultrasonic_queue)
+    ultrasonic = Ultrasonic(config["ultrasonic.pins"], ultrasonic_queue)
     controller = Controller(move, ultrasonic_queue)
     con_thread = threading.Thread(target=controller.run)
     ult_thread = threading.Thread(target=ultrasonic.start)
-    con_thread.start()
+    move.forward()
+    time.sleep(5)
+    """con_thread.start()
     ult_thread.start()
     con_thread.join()
     ult_thread.join()"""
