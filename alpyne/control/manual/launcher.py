add8ec851b6d8e832b0960b949e91ddae597fa66
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-28 23:28:16 +0200

    test camera

--- alpyne/control/manual/launcher.py
+++ alpyne/control/manual/launcher.py
@@ -3,13 +3,15 @@ import threading
 from move import Move
 from control.manual.server.receiver import Receiver
 from control.manual.server.controller import Controller
+from control.sensor.camera import Camera
 
 
 def launch(config):
     command_queue = queue.Queue()
     move = Move(config["motor.one.pins"], config["motor.two.pins"])
     receiver = Receiver(config["control.manual.bluetooth"])
-    controller = Controller(move, command_queue)
+    camera = Camera()
+    controller = Controller(move, command_queue, camera)
     con_thread = threading.Thread(target=controller.run)
     rec_thread = threading.Thread(target=receiver.get_message, args=(command_queue,))
     con_thread.start()
