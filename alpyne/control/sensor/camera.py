7c022bc0c30b1c143de15935fa90ad46701b9a3d
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-28 23:34:20 +0200

    test camera

--- alpyne/control/sensor/camera.py
+++ alpyne/control/sensor/camera.py
@@ -9,7 +9,7 @@ class Camera:
         self.camera = PiCamera()
         self.camera.resolution = (64, 64)
         self.camera.start_preview()
-        self.output_path = "resources/output/image/camera/"
+        self.output_path = "output/media/camera/"
 
     def capture(self, subdir):
         self.camera.capture(os.path.join(self.output_path, subdir, "{}.jpg".format(datetime.now())))
