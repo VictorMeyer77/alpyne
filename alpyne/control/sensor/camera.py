59c505c499225069c7386068e140f41a754fbcd5
Victor-Algo <victor.algo.contact@gmail.com>
2023-06-11 15:07:52 +0200

    add to array in camera

--- alpyne/control/sensor/camera.py
+++ alpyne/control/sensor/camera.py
@@ -3,6 +3,7 @@ from picamera import PiCamera
 import picamera.array
 import os
 
+
 class Camera:
 
     def __init__(self):
@@ -11,17 +12,11 @@ class Camera:
         self.camera.start_preview()
         self.output_path = "output/media/image/"
 
-    def capture(self, subdir):
-        self.camera.capture(os.path.join(self.output_path, subdir, "{}.jpg".format(round(time.time()*1000))))
+    def to_file(self, subdir):
+        self.camera.capture(os.path.join(self.output_path, subdir, "{}.jpg".format(round(time.time() * 1000))))
 
-    def test(self):
+    def to_array(self):
         with picamera.array.PiRGBArray(self.camera) as stream:
             self.camera.capture(stream, "rgb")
-            print(stream.array.shape)
-            print(stream.array)
-
-c = Camera()
-
-for i in range(10):
-    c.test()
-    time.sleep(1)
+            np_img = stream.array
+        return np_img
