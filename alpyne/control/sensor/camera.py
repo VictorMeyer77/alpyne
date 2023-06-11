6c5626aa61e21818ddc5f6f5728682b13796d281
Victor-Algo <victor.algo.contact@gmail.com>
2023-06-11 14:44:07 +0200

    test stream picamera

--- alpyne/control/sensor/camera.py
+++ alpyne/control/sensor/camera.py
@@ -8,17 +8,20 @@ class Camera:
     def __init__(self):
         self.camera = PiCamera()
         self.camera.resolution = (64, 64)
-        #self.camera.color_effects = (128, 128)
         self.camera.start_preview()
+        self.stream = picamera.array.PiRGBArray(self.camera)
         self.output_path = "output/media/image/"
 
     def capture(self, subdir):
         self.camera.capture(os.path.join(self.output_path, subdir, "{}.jpg".format(round(time.time()*1000))))
 
     def test(self):
-        with picamera.array.PiRGBArray(self.camera) as stream:
-            self.camera.capture(stream, "rgb")
-            print(stream.array.shape)
+        self.camera.capture(self.stream, "rgb")
+        print(self.stream.array.shape)
+        print(self.stream.array)
 
 c = Camera()
-c.test()
+
+for i in range(10):
+    c.test()
+    time.sleep(1)
